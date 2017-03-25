e1290de9ed01979a52af32ca76ee6fb61971fc9c
Alex Penketh <alex@apenketh.com>
2017-03-25 22:46:52 +0000

    Update checker.sh
    Added Yum checks, changed the kernel version checker to be more accurate.

--- checker.sh
+++ checker.sh
@@ -4,36 +4,64 @@ serverHostname=$(hostname)
 
 kernelCheck()   {
         currentKernel=$(uname -r)
+        currentKernel="kernel-$currentKernel"
         latestInstalledKernel=$(rpm -q kernel | tail -n 1)
         latestKernel=$(yum list updates kernel -q --disableexcludes=all 2>&1)
 
-        isLatestVersion()      {
-                if [ "$latestKernel" == "Error: No matching Packages to list" ] ; then
-                        echo "    Server is on the Kernel version:      $currentKernel"
-                        echo "    Latest Kernel installed is:           $latestInstalledKernel"
-                else
-                        latestNewKernel=$(echo $latestKernel | awk '{print $4}')
-                        if [ "$currentKernel" != "$latestNewKernel" ] ; then
-                                echo "    Server is on the Kernel version:          $currentKernel"
-                                echo "    Latest Kernel installed is:               $latestInstalledKernel"
-                                echo "    Latest Kernel available via download is:  $latestNewKernel"
-                        else
-                                echo ""
-                        fi
-                fi
-                                }
-
         echo "Kernel Version Status:"
         echo "---------------------"
-        if [ "$currentKernel" == "$latestInstalledKernel" ] ; then
-                echo "Server Kernel Is On The Lastest Installed Version: $currentKernel"
-                isLatestVersion;
+        if [ "$latestKernel" == "Error: No matching Packages to list" ] ; then
+                if [ "$currentKernel" == "$latestInstalledKernel" ] ; then
+                        echo "    Server Kernel Is On The Lastest Installed Version: $currentKernel"
+                else
+                        echo "    Server Kernel Is Not Running On The Latest Version"
+                        echo "        Server is on the Kernel version:      $currentKernel"
+                        echo "        Latest Kernel installed is:           $latestInstalledKernel"
+                fi
         else
-                echo "Server Kernel Is Not Running On The Latest Version"
-                isLatestVersion;
+                latestNewKernel=$(echo $latestKernel | awk '{print $4}')
+                latestNewKernel="kernel-$latestNewKernel"
+                if [ "$currentKernel" != "$latestNewKernel" ] ; then
+                        echo "    Their Is A Newer Kernel Avalible To Download"
+                        echo "        Server is on the Kernel version:          $currentKernel"
+                        echo "        Latest Kernel available via download is:  $latestNewKernel"
+                else
+                        echo ""
+                fi
         fi
                 }
 
-echo "Server : $serverHostname"
-echo ""
+yumCheck()      {
+        echo "Yum Status:"
+        echo "---------------------"
+        if [[ ! -f /var/lock/subsys/yum-cron ]]; then
+                echo "    Nightly Yum Update Is Disabled"
+        else
+                echo "    Nightly Yum Update Is Enabled"
+        fi
+
+        if [[ ! -f /var/log/yum.log ]]; then
+                echo "    Can't find the yum log to check when the last update occoured"
+        else
+                lastUpdateCheck=$(tail -n1 /var/log/yum.log | awk '{print $1, $2}')
+                echo "    The last update occoured: $lastUpdateCheck"
+        fi
+
+        echo ""
+
+        echo "    Yum Exclusions:"
+        if [[ ! -f /etc/yum.conf ]]; then
+                echo "        Cannot locate your yum configuration file to check for exclusions"
+        else
+                yumExcludesCheck=$(grep "exclude=" /etc/yum.conf)
+                echo "        You currently have the following exclusions set in your yum configuration: $yumExcludesCheck"
+        fi
+                }
+
+echo "---------------------"
+echo "Server Name: $serverHostname"
+echo "---------------------"
+yumCheck;
+echo "---------------------"
 kernelCheck;
+echo "---------------------"
