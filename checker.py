600096af29cd5d38ddec07cfc4ef1c6ca6d45a69
root <root@BAST01.Home>
2017-04-25 08:45:14 +0100

    Added in OS level checks to make sure we are running RHEL/CentOS/Fedora

--- checker.py
+++ checker.py
@@ -5,16 +5,32 @@ import os, yum, time, socket, sys, platform
 yumB = yum.YumBase()
 serverHostname = socket.gethostname()
 
+def getDist():
+	distName = platform.linux_distribution()[0]
+	global osVersion
+
+	if distName.upper() in ["RHEL", "CENTOS", "FEDORA", "CENTOS LINUX"]:
+		osVersion = distName
+	elif distName.upper() in ["DEBIAN", "UBUNTU"]:
+		print "{0} Based Systems Are Currently Not Compatible With This Script.".format(distName)
+		exit()
+	else:
+		print "Platform {0} Is Not Compatible With This Script.".format(distName)
+		exit()
+
 def yumCheck():
-        echo "Yum Status:"
-        echo "---------------------"
+        print "Yum Status:"
+        print "---------------------"
 
 def kernelCheck():
         print "Kernel Version Status:"
         print "---------------------"
+
+getDist()
 	
 print "---------------------"
 print "Server Name:", serverHostname
+print "Operating System Version:", osVersion, platform.linux_distribution()[1]
 print "---------------------"
 yumCheck()
 print "---------------------"
