39b3703a6021d807195401075c425417cbbbc9f2
root <root@BAST01.Home>
2017-04-25 19:54:28 +0100

    Added In Some Yum Checks

--- checker.py
+++ checker.py
@@ -5,6 +5,7 @@ import os, yum, time, socket, sys, platform
 yumB = yum.YumBase()
 serverHostname = socket.gethostname()
 
+
 def getDist():
 	distName = platform.linux_distribution()[0]
 	global osVersion
@@ -19,8 +20,30 @@ def getDist():
 		exit()
 
 def yumCheck():
+        yumConfFile = yumB.conf.config_file_path
+	package_list = yumB.doPackageLists(pkgnarrow='updates', patterns='', ignore_case=True)
+
         print "Yum Status:"
         print "---------------------"
+
+        if package_list.updates:
+                print 'updates available'
+                for pkg in package_list.updates:
+                        print pkg
+        else:
+                print "Their Are No Avalible Updates On This Server, You Are Up to Date"
+
+        for old in yumB.history.old():
+                if "Update" in (hpkg.state for hpkg in old.trans_data):
+                        print "The last update occurred:", time.ctime(old.beg_timestamp)
+                        break
+
+        print ""
+        print "    Yum Exclusions:"
+        if os.path.exists("{0}".format(yumConfFile)):
+                print "found it"
+        else:
+                print "        Cannot locate the yum configuration file to check for exclusions"
 
 def kernelCheck():
         print "Kernel Version Status:"
