d256d1d1151b8eb698a5322484370ce8268a82d2
root <root@BAST01.Home>
2017-05-05 12:22:41 +0100

    Added a bunch fo stuff, started testing for Kernel Checks

--- checker.py
+++ checker.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
-import os, yum, time, socket, sys, platform
+import os, yum, time, socket, sys, platform, rpm, subprocess, re
+from rpmUtils.miscutils import stringToVersion
 
 yumB = yum.YumBase()
+yumB.preconf.debuglevel = 0
 serverHostname = socket.gethostname()
 
 
@@ -18,6 +20,45 @@ def getDist():
 	else:
 		print "Platform {0} Is Not Compatible With This Script.".format(distName)
 		exit()
+
+def kernelCheck():
+	currentKernel = "kernel-" + platform.release()
+	latestInstalledKernel = subprocess.check_output(["rpm -q kernel | tail -n 1"], shell=True).strip()
+	#latestKernel = subprocess.call(["yum list updates kernel -q --disableexcludes=all"], shell=True, stdout=open(os.devnull, 'wb'))
+
+	#if latestKernel == 1:
+	#	latestKernel = "non"
+	#else:
+	#	latestKernel = subprocess.check_output(["yum list updates kernel -q --disableexcludes=all"], shell=True)
+
+        package_list = yumB.doPackageLists(pkgnarrow='updates', patterns='', ignore_case=True)
+
+	print "**test**"
+        if package_list.updates:
+                for pkg in package_list.updates:
+			if pkg == "kernel*":
+				latestKernel = pkg
+				print latestKernel
+        else:
+		latestKernel = "non"
+	print "**test**"
+
+        print "Kernel Version Status:"
+        print "---------------------"
+        if latestKernel == "non":
+                if currentKernel == latestInstalledKernel:
+                        print "    Server Kernel Is On The Latest Version: {0}".format(currentKernel)
+                else:
+                        print "    Server Kernel Is Not Running On The Latest Version"
+                        print "        Server is on the Kernel version:      {0}".format(currentKernel)
+                        print "        Latest Kernel installed is:           {0}".format(latestInstalledKernel)
+        else:
+                if currentKernel != latestNewKernel:
+                        print "    Their Is A Newer Kernel Avalible To Download:"
+                        print "        Server is on the Kernel version:          {0}".format(currentKernel)
+                        print "        Latest Kernel available via download is:  {0}".format(latestNewKernel)
+                else:
+                        print ""
 
 def yumCheck():
         yumConfFile = yumB.conf.config_file_path
@@ -45,17 +86,13 @@ def yumCheck():
         else:
                 print "        Cannot locate the yum configuration file to check for exclusions"
 
-def kernelCheck():
-        print "Kernel Version Status:"
-        print "---------------------"
-
 getDist()
 	
 print "---------------------"
 print "Server Name:", serverHostname
 print "Operating System Version:", osVersion, platform.linux_distribution()[1]
 print "---------------------"
-yumCheck()
-print "---------------------"
 kernelCheck()
+print "---------------------"
+yumCheck()
 print "---------------------"
