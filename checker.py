bf91d778139de281e5f15f69f380d3282db2fe1a
Alex P <alex@apenketh.com>
2017-06-15 11:31:37 +0100

    Few small code changes to remove unnessacry code. Further change to comply with PEP8

--- checker.py
+++ checker.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+"""
+A Python Script To Check The Server Update Status And The Currently Installed Kernel.
+https://github.com/APenketh/server-kernel-status-checker
+"""
 
 import os
 import sys
@@ -12,20 +16,22 @@ import yum
 
 yumB = yum.YumBase()
 yumB.preconf.debuglevel = 0
-serverHostname = socket.gethostname()
 
+"""Check the verion of Python running. Returns True for Python version 2.7 and above. False for 2.6 and below."""
 def checkPythonInstall():
     versionToCheck = (2, 7)
     currentVersion = sys.version_info
-    return bool(True if currentVersion[0] >= versionToCheck[0] and currentVersion[1] >= versionToCheck[1] else False)
+    return bool(True
+                if currentVersion[0] >= versionToCheck[0]
+                and currentVersion[1] >= versionToCheck[1]
+                else False)
 
+"""Checks the oeprating system type. Supported types are returned while unsupported are printed and exited."""
 def getDist():
     distName = platform.linux_distribution()[0]
-    global osVersion
-
     if distName.upper() in ["RHEL", "CENTOS", "FEDORA",
                             "CENTOS LINUX", "RED HAT ENTERPRISE LINUX SERVER"]:
-        osVersion = distName
+        return distName + " " + platform.linux_distribution()[1]
     elif distName.upper() in ["DEBIAN", "UBUNTU"]:
         print "{0} Based Systems Are Currently Not Compatible With This Script.".format(distName)
         exit()
@@ -33,15 +39,16 @@ def getDist():
         print "Platform {0} Is Not Compatible With This Script.".format(distName)
         exit()
 
+"""Performs checks agasint the Kernel & provides a logical output."""
 def kernelCheck():
     currentKernel = "kernel-" + platform.release()
-    if checkPythonInstall() == True:
+    if checkPythonInstall() is True:
         latestInstalledKernel = (
             subprocess.check_output(["rpm -q kernel | tail -n 1"], shell=True).strip())
         latestKernel = (subprocess.check_output([
             "yum list updates kernel -q --disableexcludes=all | grep -vi 'updated' | awk {'print $2'} 2>&1"
             ], shell=True, stderr=open('/dev/null', 'w')).strip())
-    elif checkPythonInstall() == False:
+    elif checkPythonInstall() is False:
         latestInstalledKernelOut = Popen(["rpm -q kernel | tail -n 1"], shell=True, stdout=PIPE)
         latestInstalledKernel = latestInstalledKernelOut.communicate()[0].strip()
         latestKernelOut = Popen([
@@ -53,12 +60,12 @@ def kernelCheck():
         if currentKernel == latestInstalledKernel:
             kcrr = "    Server Kernel Is On The Latest Version: {0}".format(currentKernel)
         else:
-	    kcrr = ("You Need To Reboot The Server To Make Use Of The Latest Kernel."
+            kcrr = ("You Need To Reboot The Server To Make Use Of The Latest Kernel."
                     + "\n    Server is on the Kernel version:\t{0}".format(currentKernel)
                     + "\n    Latest Kernel installed is:\t\t{0}".format(latestInstalledKernel))
     else:
         if currentKernel != latestKernel:
-	    kcrr = ("You Need To Download The Latest Kernel And Reboot The Server."
+            kcrr = ("You Need To Download The Latest Kernel And Reboot The Server."
                     + "\n    Server is on the Kernel version:\t{0}".format(currentKernel)
                     + "\n    Latest Kernel To Download Is:\tkernel-{0}.x86_64".format(latestKernel))
         else:
@@ -66,29 +73,34 @@ def kernelCheck():
     return kcrr
 
 class yumCheck():
+    """Checks for how many avalible updates are pending within the system."""
     def updateChecker(self):
         package_list = yumB.doPackageLists(pkgnarrow='updates', patterns='', ignore_case=True)
         totalUpdatesAv = 0
 
         if package_list.updates:
             for pkg in package_list.updates:
-	        totalUpdatesAv += 1
+                totalUpdatesAv += 1
             totalUpdatesAv = str(totalUpdatesAv) + " Updates"
         else:
             totalUpdatesAv = "Zero. You Are Up To Date"
         return totalUpdatesAv
 
+    """Gets the time and date fo the last update from yum history and returns that value."""
     def lastUpdateChecker(self):
-       	for old in yumB.history.old():
-      	    if "Update" in (hpkg.state for hpkg in old.trans_data):
+        for old in yumB.history.old():
+            if "Update" in (hpkg.state for hpkg in old.trans_data):
                 lucr = time.ctime(old.beg_timestamp)
+            else:
+                lucr = "There Is No History Of Updates"
         return lucr
 
+    """Checks for any exclusions defined within the yum configuration file."""
     def yumExChecker(self):
         yumConfFile = yumB.conf.config_file_path
-       	if os.path.exists("{0}".format(yumConfFile)):
+        if os.path.exists("{0}".format(yumConfFile)):
             if subprocess.call(["grep --quiet ^exclude=$ /etc/yum.conf"], shell=True) == False:
-	        yumExResu = "You Have No Exclusions Set Up"
+                yumExResu = "You Have No Exclusions Set Up"
             elif subprocess.call(["grep --quiet ^exclude= /etc/yum.conf"], shell=True) == False:
                 if checkPythonInstall() is True:
                     yumExResu = (subprocess.check_output([
@@ -113,17 +125,17 @@ class ThreeColTable(object):
     _ANSI_ESCAPE = re.compile(r'\x1b[^m]*m')
 
     def __init__(self, width=None):
-       	self.width = width
-       	self._left = []
-       	self._center = []
-       	self._right = []
+        self.width = width
+        self._left = []
+        self._center = []
+        self._right = []
 
     def _raw(self, s):
-       	return self._ANSI_ESCAPE.sub('', s)
+        return self._ANSI_ESCAPE.sub('', s)
 
     def _trim(self, s, maxlen, suffix='...'):
-       	rawlen = len(self._raw(s))
-       	if rawlen <= maxlen:
+        rawlen = len(self._raw(s))
+        if rawlen <= maxlen:
             return s
         markup = list(reversed([(m.start(), m.end()) for m in self._ANSI_ESCAPE.finditer(s)]))
         end = len(s)
@@ -140,20 +152,20 @@ class ThreeColTable(object):
         return s
 
     def _lines(self):
-       	height = max(len(self._left), len(self._center), len(self._right))
-       	for i in range(height):
+        height = max(len(self._left), len(self._center), len(self._right))
+        for i in range(height):
             left = center = right = ''
             if i < len(self._left):
-            	left = self._left[i]
+                left = self._left[i]
             if i < len(self._center):
-            	center = self._center[i]
+                center = self._center[i]
             if i < len(self._right):
                 right = self._right[i]
             yield (left, center, right)
 
     def _compute_width(self):
-       	mwidth = 0
-       	for l, c, r in self._lines():
+        mwidth = 0
+        for l, c, r in self._lines():
             width = len(self._raw(l))+len(self._raw(c))+len(self._raw(r))
             if c or r:
                 width += 8
@@ -161,31 +173,31 @@ class ThreeColTable(object):
         return max(self.MIN_WIDTH, min(self.MAX_WIDTH, mwidth))
 
     def left(self, s):
-       	if not isinstance(s, list):
+        if not isinstance(s, list):
             s = [str(s)]
         self._left.extend(s)
 
     def right(self, s, alignright=False):
-       	if not isinstance(s, list):
+        if not isinstance(s, list):
             s = [str(s)]
         if not alignright:
-           maxlen = max([len(self._raw(x)) for x in s])
-           s = [x + (maxlen-len(self._raw(x)))*' ' for x in s]
+            maxlen = max([len(self._raw(x)) for x in s])
+            s = [x + (maxlen-len(self._raw(x)))*' ' for x in s]
         self._right.extend(s)
 
     def line(self, left, right):
-       	self.left(left)
-       	self.right(right)
+        self.left(left)
+        self.right(right)
 
     def space(self, ruler=False):
-       	height = max(len(self._left), len(self._center), len(self._right))
-       	for col in (self._left, self._center, self._right):
+        height = max(len(self._left), len(self._center), len(self._right))
+        for col in (self._left, self._center, self._right):
             col.extend((height+1-len(col))*[''])
         if ruler:
             self._left[-1] = '---'
 
     def render(self):
-       	if self.width is None:
+        if self.width is None:
             self.width = self._compute_width()
         ret = []
         ret.append(self.width * '=')
@@ -194,8 +206,8 @@ class ThreeColTable(object):
                 ret.append(self.width * '-')
                 continue
             if not center and not right:
-            	ret.append(self._trim(left, self.width))
-            	continue
+                ret.append(self._trim(left, self.width))
+                continue
             padding1 = max(4, (self.width-len(self._raw(center)))/2-len(self._raw(left))) * ' '
             padding2 = max(4, self.width-len(self._raw(left))-len(padding1)-len(self._raw(center))-len(self._raw(right))) * ' '
             line = left + padding1 + center + padding2 + right
@@ -204,13 +216,10 @@ class ThreeColTable(object):
         return '\n'.join(ret)
 
 def overview():
-    serverHost = serverHostname
-    osP = osVersion + " " + platform.linux_distribution()[1]
     yumC = yumCheck()
-
     tbl = ThreeColTable()
 
-    tbl.line('Server Name: %s' % serverHost, 'OS Version: %s' % osP)
+    tbl.line('Server Name: %s' % socket.gethostname(), 'OS Version: %s' % getDist())
     tbl.space(ruler=True)
     tbl.left('Server Update Status:')
     tbl.left('--------------')
@@ -225,5 +234,4 @@ def overview():
     print tbl.render()
 
 if __name__ == '__main__':
-    getDist()
     overview()
